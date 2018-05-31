3d7a36c6c93ebd9c00684d2be6ec4ffb50a59bee
vruano <valentin.ruano@gmail.com>
2018-05-31 15:05:25 -0400

    Improvements and refactoring of Nucleotide.java

--- src/main/java/org/broadinstitute/hellbender/utils/Nucleotide.java
+++ src/main/java/org/broadinstitute/hellbender/utils/Nucleotide.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 import java.util.stream.LongStream;
 
 /**
- * Represents the nucleotide alphabet.
+ * Represents the nucleotide alphabet with support for IUPAC ambiguity codes.
  *
  * <p>
  *    This enumeration not only contains concrete nucleotides, but also
@@ -14,19 +14,106 @@ import java.util.stream.LongStream;
  * @author Valentin Ruano-Rubio &lt;valentin@broadinstitute.org&gt;
  */
 public enum Nucleotide {
-    A, C, G, T, N, X, INVALID;
 
-    private static final Nucleotide[] baseToValue = new Nucleotide[Byte.MAX_VALUE + 1];
+    // Basic nucleotide codes,
+    // and their one-bit-encoding masks CODE(0xTGCA):
+    A(0b0001),
+    C(0b0010),
+    G(0b0100),
+    T(0b1000),
+
+    // Extended codes:
+    // CODE(included nucs)
+    R(A,G), // Purine
+    Y(C,T), // Pyrimidines
+    S(C,G), // Strong nucletoides.
+    W(A,T), // Weak nucleotides
+    K(G,T), // Keto
+    M(A,C), // Amino
+    B(C,G,T), // Not-A (B follows A)
+    D(A,G,T), // Not-C (D follows C)
+    H(A,C,T), // Not-G (H follows G)
+    V(A,C,G), // Not-V (V follows T)
+    N(A,C,G,T), // Any/Unknown
+    X(); // Invalid.
+
+    // Uracil is considered equivalent to Thymine.
+    public static final Nucleotide U = T;
+
+    // Convenient long form alternative names for some of the enumeration values:
+
+    // Long form concrete nucleotide names.
+    @SuppressWarnings("unused")
+    public static final Nucleotide ADENINE = A;
+    @SuppressWarnings("unused")
+    public static final Nucleotide CYTOSINE = C;
+    @SuppressWarnings("unused")
+    public static final Nucleotide GUANINE = G;
+    @SuppressWarnings("unused")
+    public static final Nucleotide THYMINE = T;
+    @SuppressWarnings("unused")
+    public static final Nucleotide URACIL = U;
+
+    // Ambiguous nucleotide groups with proper long form names:
+    public static final Nucleotide STRONG = S;
+    public static final Nucleotide WEAK = W;
+    public static final Nucleotide PURINE = R;
+    public static final Nucleotide PYRIMIDINE = Y;
+    @SuppressWarnings("unused")
+    public static final Nucleotide AMINO = M;
+    @SuppressWarnings("unused")
+    public static final Nucleotide KETO = K;
+    @SuppressWarnings("unused")
+    public static final Nucleotide ANY = N;
+    @SuppressWarnings("unused")
+    public static final Nucleotide UNKNOWN = N;
+    public static final Nucleotide INVALID = X;
+
+    // actually calling values() is costly (creates a new array every time) and often we do just to find out the
+    // total number of constants.
+    private static final int NUMBER_OF_CONSTANTS;
+
+    private static final Nucleotide[] baseToValue;
+    private static final Nucleotide[] maskToValue;
 
     static {
-        Arrays.fill(baseToValue, INVALID);
-        baseToValue['a'] = baseToValue['A'] = A;
-        baseToValue['c'] = baseToValue['C'] = C;
-        baseToValue['g'] = baseToValue['G'] = G;
-        baseToValue['t'] = baseToValue['T'] = T;
-        baseToValue['u'] = baseToValue['U'] = T;
-        baseToValue['x'] = baseToValue['X'] = X;
-        baseToValue['n'] = baseToValue['N'] = N;
+        final Nucleotide[] values = values();
+        NUMBER_OF_CONSTANTS = values.length;
+        baseToValue = new Nucleotide[1 << Byte.SIZE];
+        maskToValue = new Nucleotide[16];
+        Arrays.fill(baseToValue, X);
+        for (final Nucleotide nucleotide : values) {
+            baseToValue[nucleotide.lowerCaseByteEncoding] = baseToValue[nucleotide.upperCaseByteEncoding] = nucleotide;
+            maskToValue[nucleotide.mask] = nucleotide;
+        }
+        baseToValue['u'] = baseToValue['U'] = U;
+    }
+
+    private final int mask;
+    private final boolean isConcrete;
+    private Nucleotide complement;
+    private Nucleotide transition;
+    private Nucleotide transversion;
+
+    /**
+     * Holds lower-case byte encoding for this nucleotide; {@code 0} for {@link Nucleotide#INVALID}.
+     */
+    private final byte lowerCaseByteEncoding;
+
+    /**
+     * Holds the upper-case byte encoding for this nucleotide; {@code 0} for {@link Nucleotide#INVALID}.
+     */
+    private final byte upperCaseByteEncoding;
+
+    Nucleotide(final int mask) {
+        this.mask = mask;
+        isConcrete = Integer.bitCount(mask & 0b1111) == 1;
+        lowerCaseByteEncoding = (byte) Character.toLowerCase(name().charAt(0));
+        upperCaseByteEncoding = (byte) Character.toUpperCase(name().charAt(0));
+    }
+
+    Nucleotide(final Nucleotide ... nucs) {
+        this(Arrays.stream(nucs).mapToInt(nuc -> nuc.mask).reduce((a, b) -> a | b).orElse(0));
     }
 
     /**
@@ -37,16 +124,18 @@ public enum Nucleotide {
      * <p>
      *     The {@link #INVALID} nucleotide does not have an actual base then resulting in an exception.
      * </p>
-     * @throws UnsupportedOperationException if this nucleotide does not have a byte representation such
-     *  as {@link #INVALID}.
-     * @return a positive byte value.
+     * @return a valid byte representation for a nucleotide, {@code 0} for {@link Nucleotide#INVALID}.
      */
-    public byte toBase() {
-        if (this == INVALID) {
-            throw new UnsupportedOperationException("the invalid nucleotide does not have a base byte");
-        } else {
-            return (byte) name().charAt(0);
-        }
+    public byte encodeAsByte(final boolean upperCase) {
+        return upperCase ? upperCaseByteEncoding : lowerCaseByteEncoding;
+    }
+
+    /**
+     * Returns the nucleotide encoding in a byte using its upper-case representation.
+     * @return a valid upper-case byte representation for a nucleotide, {@code 0} for {@link Nucleotide#INVALID}.
+     */
+    public byte encodeAsByte() {
+        return upperCaseByteEncoding;
     }
 
     /**
@@ -56,30 +145,198 @@ public enum Nucleotide {
      * @return never {@code null}, but {@link #INVALID} if the base code does not
      * correspond to a valid nucleotide specification.
      */
-    public static Nucleotide valueOf(final byte base) {
+    public static Nucleotide decode(final byte base) {
         return baseToValue[Utils.validIndex(base, baseToValue.length)];
+    }
+
+    public static Nucleotide decode(final char base) {
+        return decode((byte) base);
     }
 
     /**
      * Checks whether the nucleotide refer to a concrete (rather than ambiguous) base.
-     * @return
+     * @return {@code true} iff this is a concrete nucleotide.
      */
     public boolean isConcrete() {
-        return ordinal() < N.ordinal();
+        return isConcrete;
     }
 
     /**
-     * Helper class to count the number of occurrences of each nucleotide in
+     * Checks whether the nucleotide refer to an ambiguous base.
+     * @return {@code true} iff this is an ambiguous nucleotide.
+     */
+    public boolean isAmbiguous() {
+        return !isConcrete && this != INVALID;
+    }
+
+    public boolean isValid() {
+        return this != INVALID;
+    }
+
+    /**
+     * Checks whether this nucleotide code encloses all possible nucleotides for another code.
+     * @param other the other nucleotide to compare to.
+     * @return {@code true} iff any nucleotide in {@code other} is enclosed it this code.
+     */
+    public boolean includes(final Nucleotide other) {
+        Utils.nonNull(other);
+        return other != INVALID && (mask & other.mask) == other.mask;
+    }
+
+    public boolean includes(final byte b) {
+        return includes(decode(b));
+    }
+
+    public Nucleotide intersect(final Nucleotide other) {
+        return maskToValue[mask & other.mask];
+    }
+
+    /**
+     * Checks whether to base encodings make reference to the same {@link #Nucleotide}
+     *  instance regardless of their case.
+     * <p>
+     *     This method is a shorthard for:
+     *     <pre>{@link #decode}(a){@link #same(Nucleotide) same}({@link #decode}(b)) </pre>.
+     * </p>
+     *
+     *  <p>
+     *      The order of the inputs is not relevant, therefore {@code same(a, b) == same(b, a)} for any
+     *      given {@code a} and {@code b}.
+     *  </p>
+     *  <p>
+     *      Notice that if either or both input bases make reference to an invalid nucleotide (i.e. <pre> {@link #decode}(x) == {@link #INVALID}},
+     *      this method will return {@code false} even if {@code a == b}.
+     *  </p>
+     * @param a the first base to compare (however order is not relevant).
+     * @param b the second base to compare (however order is not relevant).
+     * @return {@code true} iff {@code {@link #decode}}.same({@link #decode}(b))}}
+     */
+    public static boolean same(final byte a, final byte b) {
+        return baseToValue[a] == baseToValue[b] && baseToValue[a] != INVALID;
+    }
+
+    /**
+     * Checks whether this and another {@link #Nucleotide} make reference to the same nucleotide(s).
+     * <p>
+     *     In contrast with {@link #equals}, this method will return {@code false} if any of the two, this
+     *     or the input nucleotide is the {@link #INVALID} enum value. So even <pre>{@link #INVALID}.same({@link #INVALID})</pre>
+     *     will return {@code null}.
+     * </p>
+     *
+     * @param other the other nucleotide.
+     * @return {@code true} iff this and the input nucleotide make reference to the same nucleotides.
+     */
+    public boolean same(final Nucleotide other) {
+        return this == other && this != INVALID;
+    }
+
+    /**
+     * Returns the complement nucleotide code for this one.
+     * <p>
+     *     For ambiguous nucleotide codes, this will return the ambiguous code that encloses the complement of
+     *     each possible nucleotide in this code.
+     * </p>
+     * <p>
+     *     The complement of the {@link #INVALID} nucleotide its itself.
+     * </p>
+     * @return never {@code null}.
+     */
+    public Nucleotide complement() {
+        if (complement == null) {
+            final int complementMask = ((mask & A.mask) != 0 ? T.mask : 0)
+                    | ((mask & T.mask) != 0 ? A.mask : 0)
+                    | ((mask & C.mask) != 0 ? G.mask : 0)
+                    | ((mask & G.mask) != 0 ? C.mask : 0);
+            complement = maskToValue[complementMask];
+        }
+        return complement;
+    }
+
+    /**
+     * Returns the complement for a base code.
+     * <p>
+     *     When an invalid base is provided this method will return the default encoding for the {@link #INVALID} nucleotide.
+     * </p>
+     * @param b the input base
+     * @param upperCase whether to return the uppercase ({@code true}) or the lower case ({@code false}) byte encoding.
+     * @return the complement of the input.
+     */
+    public static byte complement(final byte b, final boolean upperCase) {
+        final Nucleotide value = decode(b);
+        final Nucleotide compl = value.complement();
+        return compl.encodeAsByte(upperCase);
+    }
+
+    /**
+     * Returns the complement for a base code.
+     * <p>
+     *     The case of the output will match the case of the input.
+     * </p>
+     * <p>
+     *     When an invalid base is provided this method will return the default encoding for the {@link #INVALID} nucleotide.
+     * </p>
+     * @param b the input base
+     * @return the complement of the input.
+     */
+    public static byte complement(final byte b) {
+        return complement(b, Character.isUpperCase(b));
+    }
+
+    /**
+     * Returns the instance that would include all possible transition mutation from this one.
+     * @return never {@code null}.
+     */
+    public Nucleotide transition() {
+        if (transition == null) {
+            final int transitionMask = ((mask & A.mask) != 0 ? G.mask : 0)
+                    | ((mask & G.mask) != 0 ? A.mask : 0)
+                    | ((mask & C.mask) != 0 ? T.mask : 0)
+                    | ((mask & T.mask) != 0 ? C.mask : 0);
+            transition = maskToValue[transitionMask];
+        }
+        return transition;
+    }
+
+    /**
+     * Returns the instance that would include all possible tranversions mutation from this one.
+     * @return never {@code null}.
+     */
+    public Nucleotide transversion() {
+        if (transversion == null) {
+            final int transversionMask = ((mask & PURINE.mask) != 0 ? PYRIMIDINE.mask : 0)
+                | ((mask & PYRIMIDINE.mask) != 0 ? PURINE.mask : 0);
+            transversion = maskToValue[transversionMask];
+        }
+        return transversion;
+    }
+
+    /**
+     * Transvertion mutation toward a strong or a weak base.
+     * <p>
+     *     This method provides a non-ambiguous alternative to {@link #transversion()} for
+     *     concrete nucleotides.
+     * </p>
+     *
+     * @param strong whether the result should be a strong ({@code S: G, C}) or weak ({@code W: A, T}) nucleotide(s).
+     * @return nucleotides that may emerged from such a transversion.
+     */
+    public Nucleotide transversion(final boolean strong) {
+        return transversion().intersect(strong ? STRONG : WEAK);
+    }
+
+    /**
+     * Helper class to count the number of occurrences of each nucleotide code in
      * a sequence.
      */
     public static class Counter {
+
         private final long[] counts;
 
         /**
          * Creates a new counter with all counts set to 0.
          */
         public Counter() {
-            counts = new long[Nucleotide.values().length];
+            counts = new long[NUMBER_OF_CONSTANTS];
         }
 
         /**
@@ -97,7 +354,7 @@ public enum Nucleotide {
          * @throws IllegalArgumentException if {@code base} is {@code negative}.
          */
         public void add(final byte base) {
-            add(valueOf(base));
+            add(decode(base));
         }
 
         /**
@@ -117,7 +374,7 @@ public enum Nucleotide {
          * @throws IllegalArgumentException if {@code bases} are null or
          * it contains negative values.
          */
-        public void addAll(final byte[] bases) {
+        public void addAll(final byte ... bases) {
             Utils.nonNull(bases);
             for (final byte base : bases) {
                 add(base);
@@ -131,8 +388,14 @@ public enum Nucleotide {
             Arrays.fill(counts, 0);
         }
 
+        /**
+         * Return the total count of all nucleotide constants.
+         * @return
+         */
         public long sum() {
             return LongStream.of(counts).sum();
         }
     }
+
+
 }
